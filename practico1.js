609d527ed2fbf0d03283b6d895188a921e096700
Jose Bovi <bovi174@gmail.com>
2023-10-25 01:17:59 -0300

    primer commit

---
+++ practico1.js
@@ -0,0 +1,39 @@
+/*Ejercicio 1
+Crea dos variables, numero1 y numero2, y asígnales valores numéricos. Luego, realiza las siguientes operaciones y muestra los resultados en la consola:
+
+Suma de numero1 y numero2.
+Resta de numero1 y numero2.
+Multiplicación de numero1 y numero2.
+División de numero1 entre numero2. */
+
+var numero1 = 10;
+var numero2 = 5;
+
+console.log(numero1 + numero2);
+console.log(numero1 - numero2);
+console.log(numero1 * numero2);
+console.log(numero1 / numero2);
+
+/*
+Ejercicio 2: Concatenación de Cadenas
+
+Crea dos variables, nombre y apellido, y asígnales valores de tu elección.Luego, crea una tercera variable llamada nombreCompleto que contenga la 
+concatenación de nombre y apellido.Muestra nombreCompleto en la consola.*/
+
+var nombre = "Juan";
+var apellido = "Perez";
+var nombreCompleto = nombre + " " + apellido;
+console.log(nombreCompleto);
+
+/*Ejercicio 3: Conversión de Grados Celsius a Fahrenheit
+
+Crea una variable llamada gradosCelsius y asígnale un valor en grados Celsius. Luego, utiliza la fórmula de conversión para calcular los grados Fahrenheit 
+y almacénalos en una variable llamada gradosFahrenheit. Muestra ambos valores en la consola.*/
+
+var gradosCelsius = 10;
+var gradosFahrenheit = (gradosCelsius * 9 / 5) + 32;
+console.log(gradosFahrenheit);
+
+
+
+
